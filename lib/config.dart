d3b4b808b870f510e7236cb1d3e17ea2c02c9b90
Rodrigo <rodrigo@rodrigoti.com.br>
2025-09-29 17:24:26 -0300

    add appconfig

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+/// Arquivo de configuração que será sobrescrito pelo GitHub Actions
+/// usando `sed` no workflow flutter-build.yml
+
+class AppConfig {
+  static const String appName = "APP_NAME_PLACEHOLDER";
+  static const String apiUrl  = "API_URL_PLACEHOLDER";
+}
